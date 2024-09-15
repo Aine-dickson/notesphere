cf16f472bbd5709b6fc50682609a84506665773a
Aine-dickson <ainedixon01@gmail.com>
2024-09-15 12:41:11 +0300

    server connection

--- src/stores/api.ts
+++ src/stores/api.ts
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 // Create a new instance of Axios with custom configurations
 const api = axios.create({
-  baseURL: 'https://notesphere-sys-production.up.railway.app:3000',
-  timeout: 5000,
-  withCredentials: true
+  baseURL: 'https://notesphere-sys-production.up.railway.app/',
+  timeout: 10000,
+  // withCredentials: true
 });
 
 export default api;
